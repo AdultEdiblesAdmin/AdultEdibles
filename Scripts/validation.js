95dbaf18557e7a4f47d0aeb44100de0d0016ceda
AdultEdiblesAdmin <Craigcheney1979@gmail.com>
2021-05-12 21:47:22 -0500

    asx

--- Scripts/validation.js
+++ Scripts/validation.js
@@ -2,11 +2,13 @@
 
 function ValidateMe() {
 	
-	
-	var tenure = prompt("Please enter preferred tenure in years", "15");
+	//Please don't look at this ;') , this is a simple site for a friend, nothing vulnerable is actually stored/used on this site
+	var pass = prompt("Please enter the Admin Password", "");
 		
-	if (tenure != null) {
-		alert("You have entered " + tenure + " years" );
+	if (pass != null && pass == "AdultEdiblesAdmin") {
+		
+	} else {
+		alert(pass + " Not Accepted");
 	}
 	
 	
